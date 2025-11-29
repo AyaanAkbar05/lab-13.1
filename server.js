aaaddbe11340a89d18fb4f01b507b81267e83c49
Ayaan <ayaanakbar08@gmail.com>
2025-11-29 14:04:55 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+
+require('dotenv').config();
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const PORT = 3001;
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri);
+
+app.get('/', async (req, res) => {
+  try {
+    await client.connect();
+    res.json({ message: "Successfully connected to the database!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to connect to the database." });
+  } finally {
+    await client.close();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
